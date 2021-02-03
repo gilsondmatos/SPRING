d340f970721d4ed97cd667168b14c4f3c5084a5c
gilsondmatos <gilsondmatos@hotmail.com>
2021-02-03 11:12:48 -0300

    Renomei nome da classe

--- AVA_19_01/blogpessoal/src/main/java/org/blogpessoal/blogpessoal/Repository/UsuarioRepository.java
+++ AVA_19_01/blogpessoal/src/main/java/org/blogpessoal/blogpessoal/Repository/UsuarioRepository.java
@@ -1,12 +1,16 @@
 package org.blogpessoal.blogpessoal.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.blogpessoal.blogpessoal.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UsusarioRepository extends JpaRepository<Usuario, Long>{
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+	public List<Usuario> findAllByNomeContainingIgnoreCase (String nome);
+	public Optional<Usuario> findByUsuarioAndSenha(String usuario, String senha);
 	public Optional<Usuario> findByUsuario(String usuario);
 }
